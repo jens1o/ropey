c9e87a0080e0821b347a42bbfc6d675cb350ed29
Travis CI User <travis@example.org>
2019-01-22 07:39:40 +0000

    Documentation for cessen/ropey@5aa81d0

---
+++ source-files.js
@@ -0,0 +1,6 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex['ropey'] = {"name":"","dirs":[{"name":"tree","dirs":[],"files":["mod.rs","node.rs","node_children.rs","node_text.rs","text_info.rs"]}],"files":["crlf.rs","iter.rs","lib.rs","rope.rs","rope_builder.rs","slice.rs","str_utils.rs"]};
+sourcesIndex['ropey'] = {"name":"","dirs":[{"name":"tree","dirs":[],"files":["mod.rs","node.rs","node_children.rs","node_text.rs","text_info.rs"]}],"files":["crlf.rs","iter.rs","lib.rs","rope.rs","rope_builder.rs","slice.rs","str_utils.rs"]};
+sourcesIndex['smallvec'] = {"name":"","dirs":[],"files":["lib.rs"]};
+sourcesIndex['unreachable'] = {"name":"","dirs":[],"files":["lib.rs"]};
+sourcesIndex['void'] = {"name":"","dirs":[],"files":["lib.rs"]};
